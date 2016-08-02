2ce69e9dec6910c89bf38a0b916fc5ddd83e871d
David <goodwid@gmail.com>
2016-08-02 15:11:16 -0700

    Replaced home-grown config with npm package 'preferences'

--- command.js
+++ command.js
@@ -4,33 +4,30 @@ const hooks = require('./lib/ci-hooks');
 const close = require('./lib/close-out');
 const openGithub = require('./lib/open-github');
 const sander = require('sander');
-const {alert, alertErr, getConfigs} = require('./lib/cli-tools');
+const {alert, alertErr} = require('./lib/cli-tools');
+const Preferences = require('preferences');
+const prefs = new Preferences('tai');
 
 program
   .command('config <github_org> <github_token>')
   .description('configure github_org github_token')
   .action((github_org, github_token) => {
-    require('./lib/config.js')(sander, github_org, github_token);
+    prefs.github_org = github_org;
+    prefs.github_token = github_token;
   });
 
 program
   .command('setup <repoName>')
   .description('create branches for each team')
-  .action((repoName, options) => {
-    getConfigs()
-      .then((configs) => {
-        if (!repoName) return alertErr('plz specify repo name');
-        addBranches( repoName, configs )
-          .then( () => alert( 'branches created' ) )
-          .then( () => hooks( repoName, configs ) )
-          .then( () => alert( 'hooks complete' ) )
-          .catch( err => {
-            alertErr('error setting up repo');
-            console.log(err);
-          } );
-      })
-      .catch(err => {
-        alertErr('error retreiving configs. run config');
+  .action((repoName) => {
+    if (!prefs) return alertErr('No configuration found.  run config');
+    if (!repoName) return alertErr('No repo specified.');
+    addBranches( repoName, prefs )
+      .then( () => alert( 'branches created' ) )
+      .then( () => hooks( repoName, prefs ) )
+      .then( () => alert( 'hooks complete' ) )
+      .catch( err => {
+        alertErr('error setting up repo');
         console.log(err);
       });
   });
@@ -38,43 +35,32 @@ program
 program
   .command('close <repoName>')
   .description('merge student branches into master folders')
-  .action((repoName, options) => {
-    getConfigs()
-      .then((configs) => {
-        if (!repoName) return alertErr('plz specify repo name');
-        close(repoName, configs);
-      })
-      .catch(err => {
-        alertErr('error retreiving configs. run config');
-        console.log(err);
-      });
+  .action((repoName) => {
+    if (!prefs) return alertErr('No configuration found.  run config');
+    if (!repoName) return alertErr('No repo specified.');
+    close(repoName, prefs);
   });
 
 program
   .command('team <filepath>')
   .description('set teams to specified json filepath')
-  .action((filepath, options) => {
+  .action((filepath) => {
     sander.readFile(filepath)
       .then(data => {
         alert('setting student teams to \n' + data);
-        return sander.writeFile('.teams.json', data);
+        prefs.students = data;
       })
-      .then(() => alert('student teams set'))
-      .catch(() => alertErr('error setting teams'));
+      .catch((err) => {
+        alertErr('error setting teams');
+        console.log(err);
+      });
   });
 
 program
   .command('hub <repo_name>')
   .description('open repo on github')
-  .action((repoName, options) => {
-    getConfigs()
-      .then((configs) => {
-        openGithub(repoName, configs);
-      })
-      .catch(err => {
-        alertErr(err);
-        console.log(err);
-      });
+  .action((repoName) => {
+    openGithub(repoName, prefs);
   });
 
 program.parse(process.argv);
