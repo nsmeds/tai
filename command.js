5cd658add38ace27ac19119ab159544718698b29
martypdx <marty@martypdx.com>
2016-08-02 15:09:17 -0700

    close based on branch. lots o refactor

--- command.js
+++ command.js
@@ -1,60 +1,48 @@
 'use strict';
 const program = require('commander')
+const config = require('./lib/config');
 const addBranches = require('./lib/add-branches')
 const hooks = require('./lib/ci-hooks')
 const close = require('./lib/close-out')
 const openGithub = require('./lib/open-github')
+const { alert, alertErr } = require('./lib/cli-tools');
 const sander = require('sander');
-const {alert, alertErr, getConfigs} = require(__dirname+'/lib/cli-tools');
+const branches = require('./teams.json')
 
 program
   .command('config <github_org> <github_token>')
   .description('configure github_org github_token')
-  .action((github_org, github_token) => {
-    require(__dirname+'/lib/config.js')(sander, github_org, github_token);
-  })
+  .action(config.set);
 
 program
   .command('setup <repoName>')
+  .option('-b, --branches', 'specify a custom list of branches in json format')
   .description('create branches for each team')
-  .action((repoName, options) => {
-    getConfigs()
-      .then((configs) => {
-        if (!repoName) return alertErr('plz specify repo name');
-        addBranches( repoName, configs )
-          .then( () => alert( 'branches created' ) )
-          .then( () => hooks( repoName, configs ) )
-          .then( () => alert( 'hooks complete' ) )
-          .catch( err => {
-            alertErr('error setting up repo');
-            console.log(err);
-          } );
-      })
-      .catch(err => {
-        alertErr( 'error retreiving configs. run config' )
-        console.log(err);
-      })
+  .action(repoName => {
+    config.branches = program.branches ? JSON.parse(program.branches) : branches; 
+    addBranches( repoName, config )
+      .then( () => alert( 'branches created' ) )
+      .then( () => hooks( repoName, config ) )
+      .then( () => alert( 'hooks complete' ) )
+      .catch( err => alertErr('error setting up repo'));
   })
 
 program
   .command('close <repoName>')
   .description('merge student branches into master folders')
-  .action((repoName, options) => {
-    getConfigs()
-      .then((configs) => {
-        if (!repoName) return alertErr('plz specify repo name');
-        close(repoName, configs);
-      })
+  .action(repoName => {
+    close(repoName, config)
+      .then( () => alert(`repo "${repoName}" closed out`))
       .catch(err => {
-        alertErr('error retreiving configs. run config');
-        console.log(err);
-      })
-  })
+        alertErr('error closing repo');
+        alertErr(err);
+      });
+  });
 
 program
   .command('team <filepath>')
   .description('set teams to specified json filepath')
-  .action((filepath, options) => {
+  .action(filepath => {
     sander.readFile(filepath)
       .then(data => {
         alert('setting student teams to \n' + data);
@@ -67,16 +55,6 @@ program
 program
   .command('hub <repo_name>')
   .description('open repo on github')
-  .action((repoName, options) => {
-    getConfigs()
-      .then((configs) => {
-        openGithub(repoName, configs)
-      })
-      .catch(err => {
-        alertErr(err);
-        console.log(err);
-      })
-  })
-
+  .action(repoName => openGithub(repoName, configs));
 
 program.parse(process.argv)
---
+++ tests/tai.test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const exec = require('child_process')
+
+describe('tai', () => {
+
+    it( 'configs', done => {
+
+    })
+});
