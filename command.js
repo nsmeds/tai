ece1ae7f245894c2f81f8ef36388883ec56fb2bb
martypdx <marty@martypdx.com>
2016-08-02 16:02:08 -0700

    test through setup

--- command.js
+++ command.js
@@ -15,31 +15,6 @@ program
   .action(config.set);
 
 program
-  .command('setup <repoName>')
-  .option('-b, --branches', 'specify a custom list of branches in json format')
-  .description('create branches for each team')
-  .action(repoName => {
-    config.branches = program.branches ? JSON.parse(program.branches) : branches; 
-    addBranches( repoName, config )
-      .then( () => alert( 'branches created' ) )
-      .then( () => hooks( repoName, config ) )
-      .then( () => alert( 'hooks complete' ) )
-      .catch( err => alertErr('error setting up repo'));
-  })
-
-program
-  .command('close <repoName>')
-  .description('merge student branches into master folders')
-  .action(repoName => {
-    close(repoName, config)
-      .then( () => alert(`repo "${repoName}" closed out`))
-      .catch(err => {
-        alertErr('error closing repo');
-        alertErr(err);
-      });
-  });
-
-program
   .command('team <filepath>')
   .description('set teams to specified json filepath')
   .action(filepath => {
@@ -48,13 +23,47 @@ program
         alert('setting student teams to \n' + data);
         return sander.writeFile(__dirname, 'teams.json', data)
       })
-      .then(() => alert('student teams set'))
-      .catch(() => alertErr('error setting teams'))
+      .catch(err => alertErr(err))
+  });
+  
+program
+  .command('setup <repoName>')
+  .option('-b, --branches', 'specify a custom list of branches in json format')
+  .description('create branches for each team')
+  .action(repoName => {
+    config.branches = program.branches ? JSON.parse(program.branches) : branches;
+    config.ready()
+      .then( () => addBranches( repoName, config ) )
+      .then( () => alert( 'branches created' ) )
+      .then( () => hooks( repoName, config ) )
+      .then( () => alert( 'hooks complete' ) )
+      .catch( err => {
+        alertErr('error setting up repo');
+        alertErr(err)
+      });
   })
+
+program
+  .command('close <repoName>')
+  .description('merge student branches into master folders')
+  .action(repoName => {
+    config.ready()
+      .then( () => close(repoName, config) )
+      .then( () => alert(`repo "${repoName}" closed out`))
+      .catch(err => {
+        alertErr('error closing repo');
+        alertErr(err);
+      });
+  });
+
 
 program
   .command('hub <repo_name>')
   .description('open repo on github')
-  .action(repoName => openGithub(repoName, configs));
+  .action(repoName => {
+    config.ready()
+      .then( () => openGithub(repoName, configs) )
+
+  });
 
 program.parse(process.argv)
