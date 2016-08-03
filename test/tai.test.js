d5e04a5c66b5d544c87faa1742331a8822dd7ee5
martypdx <marty@martypdx.com>
2016-08-03 11:31:26 -0700

    couple of config tests

--- test/tai.test.js
+++ test/tai.test.js
@@ -1,15 +1,24 @@
 const assert = require('chai').assert;
-const exec = require('child_process').execSync;
+const execSync = require('child_process').execSync;
+const exec = cmd => execSync(cmd, { encoding: 'utf-8' });
 const read = require('fs').readFileSync;
 
-const tai = '../tai';
+const tai = './tai';
 
 describe('tai', () => {
 
-    it('config', done => {
-        exec(tai, 'team students.json');
-        const branches = exec(tai, 'team');
-        const expected = read('./students.json');
-        assert.equal(branches, expected);
-    })
+    it('config', () => {
+        exec('./tai clear');
+        assert.equal(exec('./tai org'), '[*] There is no current Github organization selected.\n');
+        exec('./tai config my-org my-token');
+        assert.equal(exec('./tai org'), '[*] Current selected organization is my-org\n');
+        
+    });
+
+    it('team', () => {
+        const set = exec('./tai team ./test/students.json');
+        const branches = exec('./tai team');
+        const expected = read('./test/students.json');
+        assert.include( branches, expected );
+    });
 });
